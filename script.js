97a83020a0dff28a79e088b798e51ab9a30f0bf0
Afina <afinawalton95@gmail.com>
2022-04-11 08:19:04 -0700

    Remove unused files

--- script.js
+++
@@ -1,9 +0,0 @@
-let target = document.getElementById('currentLink');
-
-function showTooltip() {
-    let tooltip = document.createElement('p');
-    tooltip.textContent = 'Series F startup in sales enablement industry';
-    tooltip.style.backgroundColor = 'green';
-}
-
-target.onmouseover = showTooltip;
